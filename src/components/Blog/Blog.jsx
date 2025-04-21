a0ab49c6c528f62700d94029d521c67e064f36ad
Sadikul Mobasshir <114331112+MOBASSHIR07@users.noreply.github.com>
2025-04-21 21:46:26 +0600

    first commit

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+
+const Blog = ({blogs}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+Blog.PropTypes = {blogs:PropTypes.object.isRequired}
+export default Blog;
